354f30acd4ba45dfa5538029a07ec30c03b17260
EMMANUEL VITOR PEREIRA DE JESUS <emmanuel.jesus@ufsb.edu.br>
2026-02-27 11:45:31 -0300

    ajuste

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import HomeView from './views/HomeView.vue'
 import './style.css'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
